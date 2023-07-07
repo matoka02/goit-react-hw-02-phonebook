10a1f8fda06c483f0690d29b687f1158a7725ca1
matoka02 <a.m.nigomedzyanova@gmail.com>
2023-07-07 20:34:50 +0300

    update

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import propTypes from 'prop-types';
+import css from './ContactList.module.css';
+
+const ContactList = ({contacts, handleDelete}) => (
+    <div>
+        <ul>
+            {contacts.map((contact, id) => (
+                <li key={id}>
+                    {contact.name}: {contact.number} 
+                    <button 
+                    type="submit" 
+                    className={css.contactListItemBtn} 
+                    onClick={() => handleDelete(contact.id)}
+                    >Delete</button>
+                </li>
+            ))}
+        </ul>
+    </div>
+);
+
+ContactList.propTypes = {
+    contacts: propTypes.arrayOf(
+        propTypes.exact({
+            id: propTypes.string.isRequired,
+            name: propTypes.string.isRequired,
+            number: propTypes.string.isRequired,
+        })
+    ),
+    handleDelete: propTypes.func.isRequired,
+};
+
+export default ContactList;
